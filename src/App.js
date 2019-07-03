1cc1f9e39ea0dfa3de048332b8b47cf281d9f160
Tam Nguyen <tam.nguyen9779@gmail.com>
2019-07-03 18:45:02 -0500

    Update license

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@material-ui/styles';
 import CountdownPage from './components/CountdownPage';
 import WelcomePage from './components/WelcomePage';
 import ReactPageScroller from "react-page-scroller";
+// import ReactFullpage from '@fullpage/react-fullpage';
 
 ReactGA.initialize('UA-143098154-1');
 ReactGA.pageview('/');
@@ -27,14 +28,24 @@ class App extends React.Component {
 		}
 	}
 
+	componentDidMount() {
+		window.addEventListener('keydown', this.goToPage);
+	}
+
 	disableScrolling = (value) => {
 		this.setState({ disableScrolling: value })
 	}
+
+	goToPage = (pageNumber) => {
+		console.log('down');
+		this.reactPageScroller.goToPage(pageNumber);
+	  }
 
 	render() {
 		return (
 			<ThemeProvider theme={theme}>
 				<ReactPageScroller
+					ref={c => this.reactPageScroller = c}
 					animationTimer={500}
 					blockScrollUp={this.state.disableScrolling ? true : false}
 					blockScrollDown={this.state.disableScrolling ? true : false}
@@ -43,6 +54,16 @@ class App extends React.Component {
 					<WelcomePage disableScrolling={this.disableScrolling} />
 				</ReactPageScroller>
 			</ThemeProvider>
+			// <ReactFullpage
+			// 	render={({ state, fullpageApi }) => {
+			// 		return (
+			// 			<ReactFullpage.Wrapper>
+			// 				<CountdownPage />
+			// 				<WelcomePage />
+			// 			</ReactFullpage.Wrapper>
+			// 		);
+			// 	}}
+			// />
 		);
 	}
 }
