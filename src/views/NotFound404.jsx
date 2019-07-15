d6214260f7f676442c3f46468b80468f84b56e3c
Tam Nguyen <tam.nguyen9779@gmail.com>
2019-07-15 03:50:53 -0500

    Update giphy link for https

--- src/views/NotFound404.jsx
+++ src/views/NotFound404.jsx
@@ -4,7 +4,7 @@ import '../style/style-views/not-found-404.css';
 function NotFound404(props) {
     return <div className="not-found">
         <div className="not-found-text">Sorry... This page doesn't exist</div>
-        <iframe src="http://tv.giphy.com/?username=illeniummusic" frameBorder="0" allowFullScreen></iframe>
+        <iframe src="https://tv.giphy.com/?username=illeniummusic" frameBorder="0" allowFullScreen></iframe>
     </div>
 };
 
