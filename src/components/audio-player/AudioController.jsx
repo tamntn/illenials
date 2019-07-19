e91e756543f5071b14bbec3a4feabd6669cea403
Tam Nguyen <tam.nguyen9779@gmail.com>
2019-07-19 15:58:27 -0500

    Audio player components

---
+++ src/components/audio-player/AudioController.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { LinearProgress } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faPlay, faPause, faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
+import AudioViewer from './AudioViewer';
+import '../../style/audio-player/audio-controller.css';
+
+const AudioProgress = withStyles({
+    root: {
+        height: 3,
+        backgroundColor: 'transparent',
+    },
+    bar: {
+        backgroundColor: '#e65100',
+    },
+})(LinearProgress);
+
+class AudioController extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            completed: 1,
+            openViewer: false,
+            isPlaying: false
+        }
+    }
+
+    openViewer = () => {
+        this.setState({ openViewer: true });
+    }
+
+    closeViewer = () => {
+        this.setState({ openViewer: false });
+    }
+
+    displayArtists = (artists) => {
+        return artists.map(artist => artist.name).join(", ");
+    }
+
+    play = (event) => {
+        event.stopPropagation();
+        this.setState({ isPlaying: true });
+    }
+
+    pause = (event) => {
+        event.stopPropagation();
+        this.setState({ isPlaying: false });
+    }
+
+    like = (event) => {
+        event.stopPropagation();
+    }
+
+    unlike = (event) => {
+        event.stopPropagation();
+    }
+
+    render() {
+        const { song_data } = this.props;
+        const { completed, openViewer, isPlaying } = this.state;
+
+        return <div className="audio-controller-wrapper">
+            <div className="mobile" onClick={() => this.openViewer()}>
+                <AudioProgress variant="determinate" value={completed} />
+                <div className="actions">
+                    {
+                        isPlaying
+                            ?
+                            <FontAwesomeIcon
+                                onClick={this.pause}
+                                icon={faPause} className="play-button" />
+                            :
+                            <FontAwesomeIcon
+                                onClick={this.play}
+                                icon={faPlay} className="play-button" />
+                    }
+                    <div className="song-info">
+                        <div className="title">{song_data.name}</div>
+                        <div className="artists">{this.displayArtists(song_data.artists)}</div>
+                    </div>
+                    <FontAwesomeIcon
+                        onClick={this.like}
+                        icon={faHeart} className="heart-button" />
+                </div>
+            </div>
+            <AudioViewer
+                open={openViewer}
+                closeViewer={this.closeViewer}
+                song_data={song_data}
+                isPlaying={isPlaying}
+                play={this.play}
+                pause={this.pause} />
+        </div>
+    }
+}
+
+export default AudioController;
---
+++ src/components/audio-player/AudioViewer.jsx
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { SwipeableDrawer, Drawer } from '@material-ui/core';
+import Slider from '@material-ui/core/Slider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faPlay, faPause, faBackward, faForward, faRandom, faHeart as faHeartSolid, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faWindowMinimize } from '@fortawesome/free-regular-svg-icons';
+import * as Vibrant from 'node-vibrant';
+import animateScrollTo from 'animated-scroll-to';
+import '../../style/audio-player/audio-viewer.css';
+
+const drawerStyles = {
+    root: {
+        backgroundColor: 'transparent',
+        overflowY: 'auto'
+    },
+    paperAnchorBottom: {
+        background: '#050102',
+        borderTop: '16px',
+        overflowY: 'auto'
+    }
+}
+
+const AudioViewerSwipableDrawer = withStyles(drawerStyles)(SwipeableDrawer);
+const AudioViewerDrawer = withStyles(drawerStyles)(Drawer);
+
+const AudioSlider = withStyles({
+    root: {
+        width: 'calc(100vw - 80px)',
+        padding: '12px 0px 0px 0px',
+        color: 'white'
+    }
+})(Slider)
+
+class AudioViewer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sliderValue: 10,
+            minimizeImage: false,
+            dominantColor: [255, 255, 255],
+            dominantColorBright: [255, 255, 255],
+            showLyrics: false,
+            isSwipable: true
+        }
+
+        this.wrapper = React.createRef();
+        this.swipableWrapper = React.createRef();
+        this.lyrics = React.createRef();
+    }
+
+    componentDidMount() {
+        const { song_data } = this.props;
+
+        if (song_data) {
+            const isSpotify = song_data.spotify_uri;
+            const artworkUrl = isSpotify ? song_data.album.artwork_url : song_data.artwork_url;
+
+            Vibrant.from(artworkUrl).getPalette()
+                .then((palette) => this.setState({
+                    dominantColor: palette.Vibrant.rgb,
+                    dominantColorBright: palette.LightVibrant.rgb
+                }))
+        }
+    }
+
+    displayArtists = (artists) => {
+        return artists.map(artist => artist.name).join(", ");
+    }
+
+    onClose = () => {
+        this.props.closeViewer();
+    }
+
+    onSliderChange = (event, value) => {
+        this.setState({
+            sliderValue: value,
+            minimizeImage: true
+        });
+    }
+
+    onSliderChangeCommitted = () => {
+        this.setState({ minimizeImage: false })
+    }
+
+    scrollToLyrics = () => {
+        this.setState({ isSwipable: false });
+
+        setTimeout(() => {
+            animateScrollTo(this.lyrics.current, { element: this.wrapper.current })
+            this.wrapper.current.addEventListener('scroll', () => {
+                if (this.wrapper.current.scrollTop === 0) {
+                    this.setState({ isSwipable: true })
+                }
+            })
+        }, 100);
+    }
+
+    setSwipeable = () => {
+        this.setState({ isSwipable: true })
+    }
+
+    renderDrawer = () => {
+        const { sliderValue, minimizeImage, dominantColor, dominantColorBright, isSwipable } = this.state;
+        const { song_data, isPlaying, play, pause } = this.props;
+        const isSpotify = song_data.spotify_uri;
+        const artworkUrl = isSpotify ? song_data.album.artwork_url : song_data.artwork_url;
+        const minimizeArtwork = (minimizeImage || !isPlaying);
+
+        return <div className="audio-viewer" ref={isSwipable ? this.swipableWrapper : this.wrapper}>
+            <div className="collapse" onClick={() => this.onClose()}>
+                <FontAwesomeIcon icon={faWindowMinimize} className="collapse-button" />
+            </div>
+            <div className="main">
+                <div className="artwork">
+                    <img
+                        className={minimizeArtwork ? "min" : "full"}
+                        style={minimizeArtwork ? null : { boxShadow: `0px 16px 72px 4px rgba(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]}, 0.35)` }}
+                        src={artworkUrl} />
+                </div>
+                <div className="slider">
+                    <AudioSlider value={sliderValue}
+                        onChange={this.onSliderChange}
+                        onChangeCommitted={this.onSliderChangeCommitted}
+                        aria-labelledby="continuous-slider" />
+                </div>
+                <div className="time">
+                    <div>{sliderValue}</div>
+                    <div>-2:48</div>
+                </div>
+                <div className="name">
+                    {song_data.name}
+                </div>
+                <div className="artists" style={{ color: `rgba(${dominantColorBright[0]}, ${dominantColorBright[1]}, ${dominantColorBright[2]}, 0.99)` }}>
+                    {this.displayArtists(song_data.artists)}
+                </div>
+                <div className="actions">
+                    <FontAwesomeIcon icon={faRandom} className="shuffle-button" />
+                    <FontAwesomeIcon icon={faBackward} className="backward-button" />
+                    {
+                        isPlaying
+                            ?
+                            <FontAwesomeIcon icon={faPause} className="play-button" onClick={pause} />
+                            :
+                            <FontAwesomeIcon icon={faPlay} className="play-button" onClick={play} />
+                    }
+                    <FontAwesomeIcon icon={faForward} className="forward-button" />
+                    <FontAwesomeIcon icon={faHeart} className="like-button" />
+                </div>
+                <div
+                    className="lyrics"
+                    ref={this.lyrics}
+                    style={{ height: `${window.innerHeight}px` }}
+                    onClick={this.scrollToLyrics}
+                >
+                    <FontAwesomeIcon icon={faArrowDown} className="lyrics-icon" /> Show Lyrics
+                </div>
+            </div>
+        </div>
+    }
+
+    render() {
+        const { isSwipable } = this.state;
+
+        return (
+            isSwipable
+                ?
+                <AudioViewerSwipableDrawer
+                    anchor="bottom"
+                    open={this.props.open}
+                    onClose={() => this.onClose()}
+                    onOpen={event => console.log(event)}
+                >
+                    {this.renderDrawer()}
+                </AudioViewerSwipableDrawer>
+                :
+                <AudioViewerDrawer
+                    anchor="bottom"
+                    open={this.props.open}
+                    onClose={() => this.onClose()}
+                >
+                    {this.renderDrawer()}
+                </AudioViewerDrawer>
+        )
+    }
+}
+
+export default AudioViewer;
